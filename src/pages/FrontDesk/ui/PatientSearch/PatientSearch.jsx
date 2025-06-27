4406b0d09a791735bd8dae060d5ff5cc20fca1ce
typshi-aswin <aswinvsivan@gmail.com>
2025-06-27 14:31:37 +0200

    fix: css changes

---
+++ src/pages/FrontDesk/ui/PatientSearch/PatientSearch.jsx
@@ -0,0 +1,11 @@
+import styles from './PatientSearch.module.css';
+
+function PatientSearch () {
+        return(
+            <div className={styles.container}>
+
+            </div>
+        );
+}
+
+export default PatientSearch;
